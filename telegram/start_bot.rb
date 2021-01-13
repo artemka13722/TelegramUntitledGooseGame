73183d7eab58c8520770937ffd973e40e933e80e
artemka <artemka.skorkin@gmail.com>
2021-01-13 16:25:55 +0700

    edit telegram key

--- telegram/start_bot.rb
+++ telegram/start_bot.rb
@@ -2,6 +2,6 @@ require File.expand_path('../config/environment', __dir__)
 require 'telegram/bot'
 require_relative 'application'
 
-token = '1410455276:AAEMJvxHgeqI426SHgeiQoSVwXjJ7EjU9nY'
+token = '1530468536:AAHvraAH0BS8rA9En3ojC56hqsJ1xPbXgio'
 
 Telegram::Bot::Client.run(token) { |bot| Application.new(bot) }
